b27d3617a873688ee8bfc78507cd2b2cfaaf4a04
jellytran80 <jellytran80@yahoo.com>
2018-01-19 18:06:29 +0700

    Create activerecord.php

---
+++ activerecord.php
@@ -0,0 +1,18 @@
+class ClassName extends \yii\db\ActiveRecord
+{
+
+   public $addition;   //what attribute you want
+
+    /* your code */
+
+   public function fields()
+   {
+        $fields = parent::fields();
+        $fields[] = 'addition';      //the value must be the same as the new attribute
+
+        /* your code */
+
+        $this->addition = 'done'
+        return $fields
+   }
+}
